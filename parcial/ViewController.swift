a03dcd5cb16fbdb8278ed721d5888c79eec51105
Vicens05 <103322542+Vicens05@users.noreply.github.com>
2022-05-28 13:34:21 -0400

    Initial Commit

---
+++ parcial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parcial
+//
+//  Created by user194452 on 5/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
